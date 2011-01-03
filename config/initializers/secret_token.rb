ca8c1f4082adeffafb714d2cd5b099603b1e0c2c
sephethus <nathan.mckaskle@gmail.com>
2011-01-03 14:56:09 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tenk::Application.config.secret_token = '5b8dd4f4d4c9d1bb4ad4c5ac5790be4f7d7a78fe3128fee5bec94784a0719e2d3bde006d87b4efab83cbb0448bfa8b3bdeee4f3a81da6e6402c668259830f7b6'
